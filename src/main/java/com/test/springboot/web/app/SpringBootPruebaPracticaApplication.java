a225b0942b71131e9114e0cca581641398957901
Diego Gamboa <diegoa.gamboa@hotmail.com>
2020-02-07 12:50:12 -0500

    created project

---
+++ src/main/java/com/test/springboot/web/app/SpringBootPruebaPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.test.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPruebaPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPruebaPracticaApplication.class, args);
+	}
+
+}
