08a8dc0cdcc7d76e8f269a0cb46b43878c32cce5
Diego Gamboa <diegoa.gamboa@hotmail.com>
2020-02-08 18:32:22 -0500

    fix

--- src/main/java/com/test/springboot/web/app/repository/IUsuarioRepositorio.java
+++ src/main/java/com/test/springboot/web/app/repository/IUsuarioRepositorio.java
@@ -1,9 +1,6 @@
 package com.test.springboot.web.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.test.springboot.web.app.models.Usuario;
 
