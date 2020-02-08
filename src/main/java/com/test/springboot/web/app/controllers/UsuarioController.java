be5a16e71d74432f3a8367f32c2e75f7af03b8b1
Diego Gamboa <diegoa.gamboa@hotmail.com>
2020-02-08 18:30:03 -0500

    test finished

--- src/main/java/com/test/springboot/web/app/controllers/UsuarioController.java
+++ src/main/java/com/test/springboot/web/app/controllers/UsuarioController.java
@@ -30,7 +30,7 @@ public class UsuarioController {
 		
 		Usuario usuario = new Usuario(nombre, apellido);
 		usuarioRepositorio.save(usuario);
-		return "index";
+		return "redirect:register";
 	}
 	
 	@GetMapping("/history")
@@ -40,9 +40,14 @@ public class UsuarioController {
 	}
 	
 	@PostMapping("users")
-	public String usersUpdate(@RequestParam(value = "usersId") Usuario[] usersId) {
+	public String usersUpdate(@RequestParam(value="usersId[]") Long[] usersId) {
+		for (int i = 0; i < usersId.length; i++) {
+			Usuario u = usuarioRepositorio.findById(usersId[i]).get();
+			u.setProcesado(true);
+			usuarioRepositorio.save(u);
+		}
 		
-		return "history";
+		return "redirect:history";
 	}
 
 }
--- src/main/java/com/test/springboot/web/app/repository/IUsuarioRepositorio.java
+++ src/main/java/com/test/springboot/web/app/repository/IUsuarioRepositorio.java
@@ -1,9 +1,14 @@
 package com.test.springboot.web.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.test.springboot.web.app.models.Usuario;
 
 public interface IUsuarioRepositorio extends JpaRepository<Usuario, Long> {
+	
+
 
 }
